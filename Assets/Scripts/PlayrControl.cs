019582c03718f59191d6a87509e38377dd7d9bd5
rohanbhatt12 <47285264+rohanbhatt12@users.noreply.github.com>
2019-02-25 12:56:54 -0500

    Add files via upload

---
+++ Assets/Scripts/PlayrControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayrControl : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed*Input.GetAxis("Horizontal")*Time.deltaTime,0f, speed*Input.GetAxis("Vertical")*Time.deltaTime);
+    }
+}
